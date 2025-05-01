1b7e2cd9c474f7299386b19010f82372f7e31784
moazhany2004 <moazhany27@gmail.com>
2025-05-01 16:05:14 +0300

    الوش الاول

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header style={{ padding: '15px 20px', background: '##FFD700' }}>
+    <header style={{ padding: '15px 20px', background: '##000' }}>
       <nav style={{ display: 'flex', gap: '20px' }}>
         <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Dashboard</Link>
         <Link to="/add" style={{ color: '#fff', textDecoration: 'none' }}>Add Transaction</Link>
